db33a7e91f2ea6414003939eaff0b75b5ebee1d9
ruddy <rcondori@ultragrupo.com>
2021-03-10 14:29:50 -0400

    clase 5 react calculadora

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import Calculadora from './components/Calculadora/Calculadora';
+
+import './index.css'
+ReactDOM.render(<Calculadora/>, document.getElementById('app'));
